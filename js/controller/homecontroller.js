8ed2fce614a166a6e8de77677641da2a139b613f
Akanksha Chandre <achand10@ncsu.edu>
2016-10-16 01:31:56 -0400

    Added dropdown

--- js/controller/homecontroller.js
+++ js/controller/homecontroller.js
@@ -4,4 +4,6 @@ angular.module('mywebsite')
 	$scope.message = 'Nothing can bring you peace but yourself';
 	$scope.hash = '3fec6363433eeb997722d07ad9e2d17c?s=80'
 
+	$scope.github = "https://github.com/akankshachandre";
+
 });
