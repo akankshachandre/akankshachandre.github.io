0d55ea83b2868d19f95adcd46706b6058331ab97
Akanksha Chandre <achand10@ncsu.edu>
2016-10-16 02:12:54 -0400

    Added dropdown for linkedIn

--- js/controller/homecontroller.js
+++ js/controller/homecontroller.js
@@ -1,13 +1,17 @@
 angular.module('mywebsite')
 .controller('homeCtrl',function($scope){
 	$scope.aboutCollapsed = false;
+	$scope.linkedInCollapsed = false;
+	$scope.githubCollapsed = false;
 
 	$scope.about = "I am hardworking,sincere,sarcastic and fun to be around";
 
-	$scope.isCollapsed = false;
+	
 	$scope.message = 'Nothing can bring you peace but yourself';
-	$scope.hash = '3fec6363433eeb997722d07ad9e2d17c?s=80'
+	$scope.hash = '3fec6363433eeb997722d07ad9e2d17c?s=80';
 
+
+	$scope.linedIn = "https://www.linkedin.com/in/akanksha-akshay-chandre-008998106";
 	$scope.github = "https://github.com/akankshachandre";
 
 });
