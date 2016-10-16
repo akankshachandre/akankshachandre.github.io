b71d6205b3764fcb0557c475ace2839a4f851d5c
Akanksha Chandre <achand10@ncsu.edu>
2016-10-16 01:23:49 -0400

    Added dropdown

--- js/controller/homecontroller.js
+++ js/controller/homecontroller.js
@@ -1,6 +1,6 @@
 angular.module('mywebsite')
 .controller('homeCtrl',function($scope){
-	$scope.isCollapsed = true;
+	$scope.isCollapsed = false;
 	$scope.message = 'Nothing can bring you peace but yourself';
 	$scope.hash = '3fec6363433eeb997722d07ad9e2d17c?s=80'
 
