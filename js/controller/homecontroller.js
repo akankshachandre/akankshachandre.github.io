2b94402e4b5072933e12f4e466fc77cce25ff8ab
Akanksha Chandre <achand10@ncsu.edu>
2016-10-16 02:01:17 -0400

    Added dropdown for about

--- js/controller/homecontroller.js
+++ js/controller/homecontroller.js
@@ -1,5 +1,9 @@
 angular.module('mywebsite')
 .controller('homeCtrl',function($scope){
+	$scope.aboutCollapsed = false;
+
+	$scope.about = "I am hardworking,sincere,sarcastic and fun to be around";
+
 	$scope.isCollapsed = false;
 	$scope.message = 'Nothing can bring you peace but yourself';
 	$scope.hash = '3fec6363433eeb997722d07ad9e2d17c?s=80'
