d274edcdd4325464b763281cfd2c4b7564b54012
Akanksha Chandre <achand10@ncsu.edu>
2016-10-19 23:43:19 -0400

    added workex

--- js/controller/homecontroller.js
+++ js/controller/homecontroller.js
@@ -19,15 +19,4 @@ angular.module('mywebsite')
 
 	$scope.name = 'Akanksha';
 
-	$scope.experiences = [];
-	var exp1 = {
-		company : 'Samsung Electronics America'
-	};
-
-	var exp2 = {
-		company : 'Accenture Services Pvt Limited'
-	};
-	$scope.experiences.push({company : 'Samsung Electronics America'});
-	$scope.experiences.push(exp2);
-
 });
