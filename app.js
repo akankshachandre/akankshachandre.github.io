7cee94d82cba0f9cf281c4666f32cd909601d4e1
Akanksha Chandre <achand10@ncsu.edu>
2016-10-20 00:01:58 -0400

    added workex

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.config(function($stateProvider,$urlRouterProvider){
 	})
 	.state('workex',{
 		url:'/workex',
-		templateUrl:'./static/workex.html',
+		templateUrl:'/static/workex.html',
 		controller:'workexCtrl'
 	})
 });
