6239c4a6077282233aac45e1d58e93256688712e
Akanksha Chandre <achand10@ncsu.edu>
2016-10-19 22:58:25 -0400

    added workex

--- js/app.js
+++ js/app.js
@@ -6,17 +6,17 @@ app.config(function($stateProvider,$urlRouterProvider){
 		url:'/home',
 		//templateUrl:'home.html',
 		controller:'homeCtrl'
-	})
+	}),
 	.state('about',{
 		url:'/about',
 		templateUrl:'../static/about.html',
 		controller:'aboutCtrl'
-	})
+	}),
 	.state('resume',{
 		url:'/resume',
 		templateUrl:'https://github.com/akankshachandre/akankshachandre.github.io/blob/master/Resume.pdf',
 		controller:'resumeCtrl'
-	})
+	}),
 	.state('workex',{
 		url:'/workex',
 		templateUrl:'../workex.html',
