0cb766d50248be222d27288fd15fbd52679f8b3e
Akanksha Chandre <akachand@cisco.com>
2018-05-20 13:55:57 -0400

    removed old files

--- js/controller/resumecontroller.js
+++
@@ -1,4 +0,0 @@
-angular.module('mywebsite')
-.controller('resumeCtrl',function($scope,$window){
-	$window.open("https://github.com/akankshachandre/akankshachandre.github.io/blob/master/Resume.pdf");
-});
